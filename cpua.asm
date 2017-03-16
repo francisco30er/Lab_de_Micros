8947ddd6827130981147838b6b5230cef5989702
francisco30er <francisco30er@gmail.com>
2017-03-15 23:33:06 -0600

    cpu

---
+++ cpua.asm
@@ -0,0 +1,26 @@
+global main
+extern system
+
+section .data
+
+
+
+
+a: db 'lscpu | grep ID;lscpu | grep Model;lscpu | grep Familia',0xA
+;a: db 'top -bn1 | grep "Cpu(s)"',0xA
+section .text
+	
+;global _start
+
+main:
+;_start:
+        push   rbp
+	mov  rbp, rsp
+	mov  rdi, a
+	call system
+
+Exit:
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
